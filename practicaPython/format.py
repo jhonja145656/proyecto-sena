a79ffcc455fb50a9909a65e07b6cf23f0c611868
JohAnAr <jhonja145656@gmail.com>
2023-05-07 02:01:06 -0500

    practica python

---
+++ practicaPython/format.py
@@ -0,0 +1,7 @@
+""""Formateo en python"""
+a = 3
+b = 4
+x = 2
+n = 4
+
+print(a, b, "resultado x*n =", x+n)
---
+++ practicaPython/intro.py
@@ -0,0 +1,5 @@
+"""Introduccion a Python"""
+
+print("Hola mundo")
+
+print("el weta " * 4)
