79145c4e14220bb4d233e6121e39b7a6920c9332
jhonja145656 <jhonja145656@gmail.com>
2023-05-12 17:10:15 -0500

    aplicando control con if

--- practicaPython/control_flujo/comparadores.py
+++ practicaPython/control_flujo/comparadores.py
@@ -5,3 +5,4 @@ print(3>=5)
 print(3==5)
 print(3=="3")
 print(3==3)
+
---
+++ practicaPython/control_flujo/if.py
@@ -0,0 +1,5 @@
+edad = 20
+if edad >= 18: 
+    print("Puedes entrar a la disco")
+else:
+    print("Eres menor de edad; No puedes Imgresar")
