05f442469b8981a8450ac21c185b255ccdc9741f
jhonja145656 <jhonja145656@gmail.com>
2023-05-12 11:56:59 -0500

    funciones matematicas

---
+++ practicaPython/numeros_funciones.py
@@ -0,0 +1,16 @@
+import math 
+
+print(round(1.3))
+print(round(1.7))
+
+print(abs(-15)) # devuelve el valor absoluto
+
+print(math.ceil(1.3)) # devuelve el numero entero superior mas cercano
+print(math.floor(1.999)) # devuelve el numero entero inferior mas cercano
+print(math.isnan(25)) # indica solo numeros reales
+print(math.pow(2,3)) # devuelve potenciacion
+print(math.sqrt(144)) # devuelve la raiz cuadrada
+
+
+
+
