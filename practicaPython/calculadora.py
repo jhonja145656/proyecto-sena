de20c7f601f58bba1e9ab8cbfced8eec04dd7304
jhonja145656 <jhonja145656@gmail.com>
2023-05-12 15:35:19 -0500

    calculadora muy basica

---
+++ practicaPython/calculadora.py
@@ -0,0 +1,23 @@
+n1 = input( "Ingresa Primer Numero")
+n2 = input( "Ingresa Segundo Numero")
+n1= int(n1)
+n2= int(n2)
+
+suma = n1 + n2
+resta = n1 - n2
+multi = n1 * n2
+divi = n1 / n2
+resto = n1 % n2
+mensaje = f""" 
+ Para los numero {n1} y {n2},
+ Elresultado de la suma es {suma}.
+ Elresultado de la resta es {resta}.
+ Elresultado de la multiplicacion es {multi}.
+ Elresultado de la divicion es {divi}.
+ Elresultado de la resto es {resto}.
+
+
+"""
+
+
+print(mensaje)
