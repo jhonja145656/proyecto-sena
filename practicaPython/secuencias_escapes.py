bb991f9758ac1504b6bde9d4cc45a2ea9acf4bf7
jhonja145656 <jhonja145656@gmail.com>
2023-05-12 10:53:52 -0500

    secuencias de scape

---
+++ practicaPython/secuencias_escapes.py
@@ -0,0 +1,9 @@
+animal = 'canino "goya"' # permite mostrar las commilas dobles
+print(animal)
+
+animal = "canino \"goya\"" # permite mostrar las commilas dobles con back slash
+print(animal)
+
+animal = 'canino \n "goya"' # \n permite realizar un salto de linea
+print(animal)
+
