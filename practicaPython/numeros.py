2716cc2e96b22dff57a8f018c19ed370dbd16433
jhonja145656 <jhonja145656@gmail.com>
2023-05-12 11:20:46 -0500

    numeros

---
+++ practicaPython/numeros.py
@@ -0,0 +1,22 @@
+numero = 2 # entero
+decimal = 1.5 # float
+imaginario = 2 + 2j # 2 + 2i
+
+# numero = numero + 2
+
+
+numero += 2
+numero -= 2
+numero *= 2
+numero /= 2
+
+
+print("numero = ", + numero)
+
+print(2+3)
+print(2-3)
+print(2*3)
+print(2/3)
+print(11//3) # devuelve solo nla parte entera
+print(2**3) # potencia
+print(2%3) # resto
