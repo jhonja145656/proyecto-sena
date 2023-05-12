11e69ea71f794b9b6a8bb615f91eac151ab4b696
jhonja145656 <jhonja145656@gmail.com>
2023-05-12 17:39:26 -0500

    ternario

--- practicaPython/control_flujo/if.py
+++ practicaPython/control_flujo/if.py
@@ -2,4 +2,9 @@ edad = 20
 if edad >= 18: 
     print("Puedes entrar a la disco")
 else:
-    print("Eres menor de edad; No puedes Imgresar")
+    print("Eres menor de edad; No puedes Imgresar") 
+
+    # operador ternario otra forma de codificar
+edad1 = 25
+mansaje = "eres mayor y rumbero !entra por favor!" if edad1 > 18 else "Eres menor de edad y No puedes entrar"
+print(mansaje)
