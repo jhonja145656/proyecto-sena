a2fc18241c17ebcad92664b627d61114f71e0ca7
JohAnAr <jhonja145656@gmail.com>
2023-05-14 18:31:26 -0500

    kwargs
    creando diccionarios

---
+++ practicaPython/funciones/kwargs.py
@@ -0,0 +1,5 @@
+def get_product(**datos):
+    print(datos)
+
+
+get_product(id="id", name="iphone", desc="telefono iphone 14")
---
+++ practicaPython/funciones/return.py
@@ -0,0 +1,9 @@
+def suma(a, b):
+    resultado = a + b
+    return resultado
+
+
+c = suma(4, 5)
+d = suma(c, 5)
+
+print(d,)
