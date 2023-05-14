43178105daa538c20506646a78d5c70e78d9733c
JohAnAr <jhonja145656@gmail.com>
2023-05-13 21:47:51 -0500

    Update while.py
    iterando con while y convirtiendo a minuscula los string

--- practicaPython/control_flujo/while.py
+++ practicaPython/control_flujo/while.py
@@ -1,4 +1,9 @@
-numero = 1
-while numero < 100:
-    print(numero)
-    numero *= 2
+# numero = 1
+# while numero < 100:
+# print(numero)
+# numero *= 2
+
+comando = ""
+while comando.lower() != "salir":
+    comando = input("$ ")
+    print(comando)
