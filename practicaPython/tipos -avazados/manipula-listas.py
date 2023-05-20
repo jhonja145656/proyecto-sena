5e6a2a2812ad0bc62611e3680c5b58518972ab02
JohAnAr <jhonja145656@gmail.com>
2023-05-20 02:09:05 -0500

    Create manipula-listas.py
    manipulando listas

---
+++ practicaPython/tipos -avazados/manipula-listas.py
@@ -0,0 +1,12 @@
+Mascotas = ["goya", "linux", "rayito", "rocket"]
+print(Mascotas[0])
+Mascotas[0] = "Bicho"
+# print(Mascotas)
+# print(Mascotas[0:3])
+# print(Mascotas[1::2])  # muestra de 2 en 2 saltando un elemento
+
+numeros = list(range(21))
+print(numeros[::2])  # pares
+
+numeros = list(range(21))
+print(numeros[1::2])  # Impares
