9115c862475503e90d0e1c614bfae0be1ed1049b
JohAnAr <jhonja145656@gmail.com>
2023-05-31 21:29:39 -0500

    hora actual

---
+++ practicaPython/tipos -avazados/area-circunferencia.py
@@ -0,0 +1,5 @@
+import math
+
+radio = float(input("Ingrese el radio del circulo:"))
+area = float(math.pi * radio**2)
+print("El area del circulo con radio de {} tiene un area de {}".format(radio, area,))
