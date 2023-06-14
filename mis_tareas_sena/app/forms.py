e088b2a198cba5c32bc4390f801c939e0caaa95b
JohAnAr <jhonja145656@gmail.com>
2023-06-13 20:15:21 -0500

    mis taresa

--- mis_tareas_sena/app/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms.fields import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
-
-
-class LoginForm(FlaskForm):
-    username = StringField('Nombre de usuario', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Enviar')
